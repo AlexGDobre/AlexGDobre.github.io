c1c57f31466172e94ad394ecd903533793055d13
AlexHDobre <blakseaok@gmail.com>
2022-09-28 16:25:04 +0300

    done

--- fe-pro-27092022/l1/js.js
+++ fe-pro-27092022/l1/js.js
@@ -1,32 +1,34 @@
 let operation = prompt("Что сделать? - добавить.отнять.умножить.разделить?");
 let aValue = prompt("Первое значение");
-let aValueData = parseInt(aValue);
+let aValueData = parseFloat(aValue);
 let bValue = prompt("Второе значение");
-let bValueData = parseInt(bValue);
-// console.log(operation);
-console.log(aValue);
-console.log(bValue);
+let bValueData = parseFloat(bValue);
+console.log("операция "+operation);
+console.log("первое значение "+aValue);
+console.log("второе значение "+bValue);
 
-console.log("---if section---")
-if (operation == "добавить"){
-  result = aValueData + bValueData;
+console.log("---if-выбор---")
+if(operation == "добавить"){
+  resultplus = aValueData + bValueData;
+  result = "Добавили и получили " + resultplus;
 }
 if(operation=="отнять"){
-  result = aValueData - bValueData;
+  resultminus = aValueData - bValueData;
+  result = "Отняли и получили " + resultminus;
 }
 if(operation=="умножить"){
-  result = aValueData * bValueData;
+  resultmultiply = aValueData * bValueData;
+  result = "Умножили и получили " + resultmultiply;
 }
 if(operation=="разделить"){
-  result =  aValueData / bValueData;
+  resultdivide =  aValueData / bValueData;
+  result= "Разделили и получили " + resultdivide;
 }
 console.log(result);
-// console.log(resultminus);
 
 
 
 console.log("-------------------");
-
 // let userData = prompt('Ваш вік?', '0');
 // let procesedUserData = parseInt(userData);
 // console.log("Вік юзера ", userData);
---
+++ fe-pro-27092022/l1/name.js
@@ -0,0 +1,21 @@
+let firstName, secondName;
+firstName=prompt("YourFirstNameplz");
+secondName=prompt("YourSecondNameplz");
+console.log(firstName);
+console.log(secondName);
+document.writeln("Первое имя: "+firstName,
+"Второе имя: "+secondName+" .как на некст строку вывести? хз. но думаю скоро узнаю");
+
+// document.getElementById("firstNameData").innerHTML=(firstName);
+// document.getElementById("secondNameData").write=(secondName);
+
+
+let country = prompt("С какой вы страны? uk/ru");
+let countryData = country!="ru" && country=="uk";
+console.log(countryData);
+if(countryData == false){
+  console.log("Ой вы из Англии. наши сервера к вам не достают");
+}
+if(countryData == true){
+  console.log("зисис а тест. проходите");
+}
